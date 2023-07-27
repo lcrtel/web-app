70c0fd43f597377da25f8534115de96371e68a08
Noomym <pkmymoonpk@gmail.com>
2023-07-28 00:35:42 +0530

    fix

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,15 +1,14 @@
-// import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-// import { cookies } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-// export async function GET(request: NextRequest) {
-//     const requestUrl = new URL(request.url);
-//     const code = requestUrl.searchParams.get("code");
-
-//     if (code) {
-//         const supabase = createRouteHandlerClient<Database>({ cookies });
-//         await supabase.auth.exchangeCodeForSession(code);
-//     }
-
-//     return NextResponse.redirect("/dashboard");
-// }
+export async function GET(request: Request) {
+    // const requestUrl = new URL(request.url);
+    // const code = requestUrl.searchParams.get("code");
+    // if (code) {
+    //     const supabase = createRouteHandlerClient<Database>({ cookies });
+    //     await supabase.auth.exchangeCodeForSession(code);
+    // }
+    // // URL to redirect to after sign in process completes
+    // return NextResponse.redirect(requestUrl.origin);
+}

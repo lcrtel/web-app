0635d5c7bd541eec836b0bf9462811019eca8898
Noomym <pkmymoonpk@gmail.com>
2023-07-28 00:09:51 +0530

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { FetchUserRole } from "@/utils/user";
+// import { FetchUserRole } from "@/utils/user";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
--- utils/user.js
+++ utils/user.js
@@ -1,20 +1,20 @@
-export const FetchUserRole = async (supabase) => {
-    let { data: user } = await supabase.from("profiles").select("*");
+// export const FetchUserRole = async (supabase) => {
+//     let { data: user } = await supabase.from("profiles").select("*");
 
-    if (user && user.length > 0) {
-        const userRoleId = user[0].role_id;
-        let { data: role } = await supabase
-            .from("roles")
-            .select("name")
-            .eq("id", userRoleId);
-        return role[0].name;
-    }
+//     if (user && user.length > 0) {
+//         const userRoleId = user[0].role_id;
+//         let { data: role } = await supabase
+//             .from("roles")
+//             .select("name")
+//             .eq("id", userRoleId);
+//         return role[0].name;
+//     }
 
-    return;
-};
+//     return;
+// };
 
-export const FetchUserData = async (supabase) => {
-    let { data: user } = await supabase.from("profiles").select("*");
+// export const FetchUserData = async (supabase) => {
+//     let { data: user } = await supabase.from("profiles").select("*");
 
-    return user;
-};
+//     return user;
+// };

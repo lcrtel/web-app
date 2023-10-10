82e3f1c014ccf96a7e20985fa4f145b47e1b0f49
Mymoon PK <62475918+pkmymoon@users.noreply.github.com>
2023-10-10 11:02:45 +0530

    vercel analytics setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Toast from "./Toast";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
             <body className={`${inter.className} text-primary-500 `}>
                 <Toast />
                 <main> {children} </main>
+                <Analytics />
             </body>
         </html>
     );

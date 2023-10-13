2dae8bf2d3188f60d81194a80e707f75eb44d702
Mymoon PK <62475918+pkmymoon@users.noreply.github.com>
2023-10-13 10:55:33 +0530

    opengraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,4 +1,5 @@
 import { ImageResponse } from "next/server";
+import Image from "next/image";
 
 // Route segment config
 export const runtime = "edge";
@@ -13,25 +14,41 @@ export const size = {
 export const contentType = "image/png";
 
 // Image generation
-export default async function Image() {
+export default async function ImageGeneration() {
   
 
     return new ImageResponse(
         (
             // ImageResponse JSX element
-            <div
-                style={{
-                    fontSize: 128,
-                    background: "white",
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-            >
-                LCRTelcom
+            <div className="relative">
+                <Image
+                    src="/blue_wave_bg.jpg"
+                    width={2000}
+                    height={2000}
+                    alt="Background Image"
+                />
+                <div className=" h-full w-full flex flex-col items-center justify-center absolute gap-4 inset-0">
+                    <Image
+                        src="/lcrtelcom_logo.svg"
+                        width={200}
+                        height={50}
+                        className=" mb-5"
+                        alt="LCRTel Logo"
+                    />
+                    <div className="flex flex-col items-start sm:items-center bg-white bg-opacity-25 backdrop-blur p-5 md:p-10 rounded-2xl sm:rounded-3xl border-2 border-white">
+                        <h1 className="text-4xl sm:text-center font-bold tracking-tight text-primary-500 sm:text-5xl">
+                            Streamline Your
+                            <br />
+                            VoIP Trading Experience
+                        </h1>
+                        <p className="mt-6 text-lg sm:text-center leading-8 max-w-2xl text-primary-500">
+                            Unlock new possibilities and maximize your VoIP
+                            trading potential with LCR Telcom&apos;s
+                            cutting-edge platform.
+                        </p>
+                    </div>
+                </div>
             </div>
-        ),
+        )
     );
 }

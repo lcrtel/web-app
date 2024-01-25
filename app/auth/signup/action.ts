ec88a330afb6fda82ea612494555585521e8880c
lcrtel <lcrtelweb@gmail.com>
2024-01-25 12:44:25 +0530

    fix: signup email

--- app/auth/signup/action.ts
+++ app/auth/signup/action.ts
@@ -4,7 +4,7 @@ import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const signUp = async (formData: any) => {
-    "use server";
+    ("use server");
 
     const origin = headers().get("origin");
     const supabase = supabaseServer();
@@ -33,5 +33,17 @@ export const signUp = async (formData: any) => {
         return redirect(`/auth/signup?message=${error.message}`);
     }
 
-    return redirect("/auth/login?message=Check email to continue sign in process");
+    fetch(`/api/emails/auth/signup`, {
+        method: "POST",
+        body: JSON.stringify({
+            name: formData.name,
+            company_name: formData.company_name,
+            email: formData.email,
+            password: formData.password,
+        }),
+    });
+
+    return redirect(
+        "/auth/login?message=Check email to continue sign in process"
+    );
 };

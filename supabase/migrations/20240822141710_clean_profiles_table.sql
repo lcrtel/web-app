5b203864f1e7eead600616b35fafdf3ed5c9b208
lcrtel <lcrtelweb@gmail.com>
2024-08-22 20:01:42 +0530

    removes unwanted columns from profiles table

---
+++ supabase/migrations/20240822141710_clean_profiles_table.sql
@@ -0,0 +1,15 @@
+alter table "public"."profiles" drop column "agent_id";
+
+alter table "public"."profiles" drop column "balance";
+
+alter table "public"."profiles" drop column "departments";
+
+alter table "public"."profiles" drop column "over_draft";
+
+alter table "public"."profiles" drop column "payment_method";
+
+alter table "public"."profiles" drop column "vos_client_id";
+
+alter table "public"."profiles" drop column "vos_vendor_id";
+
+

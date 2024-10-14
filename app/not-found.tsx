f0eda6fa8800c33f6552b7ca02051304fe413dcd
lcrtel <lcrtelweb@gmail.com>
2024-10-14 14:40:04 +0530

    note found page fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,9 @@
-"use client";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export default function NotFound() {
-  useEffect(() => {
-    
-  })
   return (
-    <div className="flex flex-col h-screen items-center gap-2 justify-center">
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-9xl font-extrabold">404</h1>
       <h2 className="text-3xl font-bold">Not Found</h2>
       <p className="text-lg font-medium">Could not find requested resource</p>

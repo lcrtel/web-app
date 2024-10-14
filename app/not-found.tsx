fb50f73f5f6c86a9a64b4e75ec104448b33867aa
lcrtel <lcrtelweb@gmail.com>
2024-10-14 14:39:38 +0530

    not found page update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,20 @@
+"use client";
+import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function NotFound() {
+  useEffect(() => {
+    
+  })
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col h-screen items-center gap-2 justify-center">
+      <h1 className="text-9xl font-extrabold">404</h1>
+      <h2 className="text-3xl font-bold">Not Found</h2>
+      <p className="text-lg font-medium">Could not find requested resource</p>
+      <Link href="/" className={buttonVariants({ variant: "default" })}>
+        Return Home
+      </Link>
     </div>
   );
 }
